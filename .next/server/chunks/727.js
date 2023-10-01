d8be7738ed60060ae7896b1850d96f003acc8ef7
Gagan Arora <arorag2577@gmail.com>
2023-10-02 00:07:22 +0530

    typo fix

--- .next/server/chunks/727.js
+++
@@ -1,1 +0,0 @@
-"use strict";exports.id=727,exports.ids=[727],exports.modules={83323:(e,t,r)=>{e.exports=r(30517)},95655:()=>{"getAll"in Headers.prototype||(Headers.prototype.getAll=function(e){if("set-cookie"!==(e=e.toLowerCase()))throw Error("Headers.getAll is only supported for Set-Cookie header");let t=[...this.entries()].filter(([t])=>t===e);return t.map(([,e])=>e)})}};
